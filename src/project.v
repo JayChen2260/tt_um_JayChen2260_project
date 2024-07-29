49cd2f16b7a063ece4e1db3419bb0b8db193606c
CL-123-abc <lciecen@gmail.com>
2024-07-29 11:17:02 -0700

    Update project.v

--- src/project.v
+++ src/project.v
@@ -3,7 +3,8 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 `default_nettype none
-module tt_um_JayChen2260_project (// Dedicated inputs
+module tt_um_JayChen2260_project (
+    input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
     output wire [7:0] uio_out,  // IOs: Output path
